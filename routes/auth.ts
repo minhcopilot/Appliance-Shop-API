f2b978d03930a485056a6c1a0d43a7a260c29c10
minh phan <21115053120333@sv.ute.udn.vn>
2024-03-24 19:24:37 +0700

    feat: Add Encrypt and hash user passwords upon registration

--- routes/auth.ts
+++ routes/auth.ts
@@ -7,13 +7,20 @@ const { generateToken, generateRefreshToken } = require('../utils/jwtHelper');
 import { Customer } from '../entities/customer.entity';
 const repository = AppDataSource.getRepository(Customer);
 const passport = require('passport');
+import * as bcrypt from 'bcrypt';
+
+// Define a new interface extending Request
+interface AuthenticatedRequest extends Request {
+  user?: any; // Define user property
+}
 
 //POST login with jwt token
-router.post('/login', passport.authenticate('local', { session: false }), async (req: Request, res: Response, next: NextFunction) => {
+router.post('/login', passport.authenticate('local', { session: false }), async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
-    const data = req.body;
-    const token = generateToken(data);
-    const refreshToken = generateRefreshToken(data);
+    const authenticatedUser = req.user;
+    const token = generateToken(authenticatedUser);
+    const user: any = await repository.findOneBy({ email: authenticatedUser.email });
+    const refreshToken = generateRefreshToken(user.id);
     return res.status(200).json({ token, refreshToken });
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error', error: error });
@@ -23,20 +30,34 @@ router.post('/login', passport.authenticate('local', { session: false }), async 
 
 router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const data = req.body;
-    const customer = await repository.findOneBy({ email: data.email });
+    const { firstName, lastName, phoneNumber, address, birthday, email, password } = req.body;
+    const customer = await repository.findOneBy({ email: email });
     if (customer) {
       return res.status(400).json({ message: 'Account already exists' });
     }
-    let newCustomer = new Customer();
-    newCustomer = {
-      ...newCustomer,
-      ...req.body,
+    const hash = await bcrypt.hash(password, 10);
+    const newCustomer = {
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      address: address,
+      birthday: birthday,
+      email: email,
+      password: hash,
     };
     await repository.save(newCustomer);
-    const token = generateToken(data);
-    const refreshToken = generateRefreshToken(data);
-    return res.status(200).json({ message: 'Register successfully', newCustomer, token, refreshToken });
+    const tokenCustomer = {
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      address: address,
+      birthday: birthday,
+      email: email,
+    };
+    const token = generateToken(tokenCustomer);
+    const user: any = await repository.findOneBy({ email: email });
+    const refreshToken = generateRefreshToken(user.id);
+    return res.status(200).json({ message: 'Register successfully', Customer: tokenCustomer, token, refreshToken });
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error', error: error });
   }
@@ -52,11 +73,19 @@ router.post('/refresh-token', async (req: Request, res: Response, next: NextFunc
       if (err) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-      const { email } = user;
-      const customer = await repository.findOneBy({ email: email });
+      const { id } = user;
+      const customer = await repository.findOneBy({ id: id });
       if (customer) {
-        const data = customer;
-        const token = generateToken(data);
+        const { firstName, lastName, phoneNumber, address, birthday, email } = customer;
+        const tokenCustomer = {
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          address: address,
+          birthday: birthday,
+          email: email,
+        };
+        const token = generateToken(tokenCustomer);
         return res.status(200).json({ token: token });
       }
       return res.status(401).json({ error: 'Unauthorized' });
