733ff4e8034994a8b3cae0e45cca52abdc4d1ce8
Vuong Le <vuongle.dev@gmail.com>
2024-06-25 22:09:30 +0700

    update redeploy script to check for change before redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,13 +1,27 @@
-echo "Pulling from git..."
-git pull
-echo "Rebuilding..."
-/www/server/nodejs/v18.20.0/bin/yarn build
-echo "Restarting project"
-pid=`ps aux|grep $(cat /www/server/nodejs/vhost/pids/xxx.pid)|grep -v grep|wc -l`
-echo ${pid}
-echo 
-if [ "${pid}" == "0" ];then
-# bash /www/server/nodejs/vhost/scripts/xxx.sh
-echo "${pid} restarted"
-else echo "Server not started, started"
-fi 
+git remote update
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Pulling from git..."
+    git pull
+    echo "Rebuilding..."
+    /www/server/nodejs/v18.20.0/bin/yarn build
+    echo "Restarting project"
+    pid=`ps aux|grep $(cat /www/server/nodejs/vhost/pids/xxx.pid)|grep -v grep|wc -l`
+    echo ${pid}
+    if [ "${pid}" == "0" ];then
+        # bash /www/server/nodejs/vhost/scripts/xxx.sh
+        echo "${pid} restarted"
+        else echo "Server not started, started"
+    fi 
+elif [ $REMOTE = $BASE ]; then
+    echo "Code on server has been edited, please check again"
+else
+    echo "Diverged"
+fi
+
